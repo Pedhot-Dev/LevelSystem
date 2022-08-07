1301d5986ba6ceba7f6373b6e295bd791a270005
Pedhot-Dev <wanzgamerz2@gmail.com>
2022-08-07 09:53:23 +0700

    update LevelManager.php

--- src/PedhotDev/LevelSystem/level/LevelManager.php
+++ src/PedhotDev/LevelSystem/level/LevelManager.php
@@ -6,13 +6,19 @@ use PedhotDev\LevelSystem\Loader;
 
 class LevelManager {
 
+    private Level $levelClass;
+
+    public function __construct() {
+        $this->levelClass = new Level();
+    }
+
     public function saveAll() {
         $data = Loader::getInstance()->data;
         $data->setAll($this->getLevelClass()->levels);
     }
 
     public function getLevelClass(): Level {
-        return new Level();
+        return $this->levelClass;
     }
 
 }
